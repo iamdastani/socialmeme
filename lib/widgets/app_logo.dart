db05a0394e9f839636d703134b9460d4cc1dffb8
iamdastani <iamdastani@gmail.com>
2023-12-01 16:25:46 +0300

    Faker Dependecy Configuration

--- lib/widgets/app_logo.dart
+++ lib/widgets/app_logo.dart
@@ -9,7 +9,7 @@ class AppLogo extends StatelessWidget {
     final textTheme = theme.textTheme;
 
     return Text(
-      'SocialMeme😄',
+      "SocialMeme😄",
       style: textTheme.headlineMedium?.copyWith(
         fontWeight: FontWeight.bold,
         color: theme.colorScheme.primary,
